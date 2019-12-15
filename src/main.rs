9e3e3210dfba4ec543f5579b754c6864d5625aa3
michael-grunder <michael.grunder@gmail.com>
2019-12-14 19:54:45 -0800

    Simplify example

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,40 @@
-use futures::{executor::block_on, prelude::*, task};
+use futures::prelude::*;
 use redis_async::client::connect::RespConnection;
 use redis_async::{client, resp_array};
-use std::{error::Error, io::Read};
+use std::error::Error;
 
 async fn get_connection(port: u16) -> Result<RespConnection, std::io::Error> {
     let addr = format!("127.0.0.1:{}", port).parse().unwrap();
     client::connect(&addr).await
 }
 
-async fn next_reply(
-    con: &mut RespConnection,
-) -> Option<Result<redis_async::resp::RespValue, redis_async::error::Error>> {
-    con.next().await
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let mut c1 = get_connection(6379).await?;
+    let port1 = std::env::args()
+        .nth(1)
+        .unwrap_or_else(|| "6379".to_string())
+        .parse()
+        .unwrap();
+
+    let port2 = std::env::args()
+        .nth(2)
+        .unwrap_or_else(|| "6379".to_string())
+        .parse()
+        .unwrap();
+
+    let mut c1 = get_connection(port1).await?;
     c1.send(resp_array!["MONITOR"]).await?;
 
-    let mut skip = c1.skip(1);
-    while let Some(resp) = skip.next().await {
-        println!("Response: {:?}", resp);
+    let mut c2 = get_connection(port2).await?;
+    c2.send(resp_array!["MONITOR"]).await?;
+
+    // Skip the "OK" response for each "MONITOR" command
+    let mut skip1 = c1.skip(1);
+    let mut skip2 = c2.skip(1);
+
+    // Simple enough to wait on one stream of replies
+    while let Some(reply) = skip1.next().await {
+        println!("Reply: {:?}", reply);
     }
 
     Ok(())
